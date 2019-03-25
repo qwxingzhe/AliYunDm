10af40efa552de070461ae0ef973aaf66fd65b17
whalephp <qwei@whalephp.cn>
2019-03-25 08:58:08 +0800

    update

--- send2.php
+++ send2.php
@@ -1,10 +1,7 @@
-<?php
+<?php    
+include_once './AliDm.php';
 
-include_once 'src/AliYunDm.php';
-
-use whalephp\tool\AliYunDm;
-
-$aliYunDm = new AliYunDm("xxxxxxxxx", "xxxxxxxxxxx", "xxxxxxxxxxxxxxxxxxxxxxxxxx");
+$aliYunDm = new AliYunDm("xxxxxx", "xxxxxx", "xxxxxxxxxxxxxxxxxxxx");
 $aliYunDm->sendMail([
 	'subject'	=> "这是标题",
 	'body'		=> "哒哒哒<br/>啦啦啦啦啦<b>平台</b>的啊",
--- src/AliYunDm.php
+++ src/AliYunDm.php
@@ -1,6 +1,7 @@
 <?php
+namespace whalephp\tool;
 
-include_once 'aliyun-php-sdk-core/Config.php';
+include_once dirname(dirname(__FILE__)) . '/aliyun-php-sdk-core/Config.php';
 use Dm\Request\V20151123 as Dm;                
 
 class AliYunDm
@@ -18,16 +19,15 @@ class AliYunDm
 	
 	public function sendMail($opt)
 	{
-		//需要设置对应的region名称，如华东1（杭州）设为cn-hangzhou，新加坡Region设为ap-southeast-1，澳洲Region设为ap-southeast-2。
+        //需要设置对应的region名称，如华东1（杭州）设为cn-hangzhou，新加坡Region设为ap-southeast-1，澳洲Region设为ap-southeast-2。
 		//$iClientProfile = DefaultProfile::getProfile("cn-hangzhou", "<your accessKey>", "<your accessSecret>");        
-		$iClientProfile = DefaultProfile::getProfile($this->regionId, $this->accessKeyId, $this->accessSecret);    
-		
-		
-		//新加坡或澳洲region需要设置服务器地址，华东1（杭州）不需要设置。
+		$iClientProfile = \DefaultProfile::getProfile($this->regionId, $this->accessKeyId, $this->accessSecret);
+
+        //新加坡或澳洲region需要设置服务器地址，华东1（杭州）不需要设置。
 		//$iClientProfile::addEndpoint("ap-southeast-1","ap-southeast-1","Dm","dm.ap-southeast-1.aliyuncs.com");
 		//$iClientProfile::addEndpoint("ap-southeast-2","ap-southeast-2","Dm","dm.ap-southeast-2.aliyuncs.com");
-		$client = new DefaultAcsClient($iClientProfile);    
-		$request = new Dm\SingleSendMailRequest();     
+		$client = new \DefaultAcsClient($iClientProfile);
+		$request = new Dm\SingleSendMailRequest();
 		//新加坡或澳洲region需要设置SDK的版本，华东1（杭州）不需要设置。
 		//$request->setVersion("2017-06-22");
 		//$request->setAccountName("控制台创建的发信地址");
@@ -46,11 +46,11 @@ class AliYunDm
 			$response = $client->getAcsResponse($request);
 			print_r($response);
 		}
-		catch (ClientException  $e) {
+		catch (\ClientException  $e) {
 			print_r($e->getErrorCode());   
 			print_r($e->getErrorMessage());   
 		}
-		catch (ServerException  $e) {        
+		catch (\ServerException  $e) {
 			print_r($e->getErrorCode());   
 			print_r($e->getErrorMessage());
 		}
